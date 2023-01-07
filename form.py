36fe3d797f3b617f13f8f37c65714ca7c610a0e5
MAUdatos <121821660+MAUdatos@users.noreply.github.com>
2023-01-07 18:11:01 -0300

    Update form.py

--- form.py
+++ form.py
@@ -17,5 +17,5 @@ with st.form(key="my_form",clear_on_submit=True):
         st.write("Note", note_input, "stock_ticker", stock_ticker_input)
         form_callback(stock_ticker_input,note_input)
 
-st.info(" #### Show contents of the CSV file :point_down:")
-st.dataframe(pd.read_csv("notes.csv",names=["Stock","Note"]),height=300)
+#st.info(" #### Show contents of the CSV file :point_down:")
+#st.dataframe(pd.read_csv("notes.csv",names=["Stock","Note"]),height=300)
