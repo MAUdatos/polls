d1d2d4632fac34760e91fae4ab6c8479883ad7c7
MAUdatos <121821660+MAUdatos@users.noreply.github.com>
2023-01-07 18:58:34 -0300

    Update form.py

--- form.py
+++ form.py
@@ -1,13 +1,1 @@
-import csv  
 
-header = ['name', 'area', 'country_code2', 'country_code3']
-data = ['Afghanistan', 652090, 'AF', 'AFG']
-
-with open('data.csv', 'w', encoding='UTF8') as f:
-    writer = csv.writer(f)
-
-    # write the header
-    writer.writerow(header)
-
-    # write the data
-    writer.writerow(data)
