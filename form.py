64620d7270ddcd8c29b9c24900e34939025a04a5
MAUdatos <121821660+MAUdatos@users.noreply.github.com>
2023-01-07 18:39:06 -0300

    Update form.py

--- form.py
+++ form.py
@@ -1,28 +1,10 @@
-import pandas as pd
-import streamlit as st
+import csv
+ 
+Nombre = "An"
+Nota = None
 
-FILE_TYPES = ["csv"]
-
-file = st.file_uploader("Upload .csv file", type=FILE_TYPES)
-if file is not None:
-    data = pd.read_csv(file)
-    st.dataframe(data.head(10))
-    
-    dataframe = {
-        "DateTime": ["ss"],
-        "Name": ["s"],
-        "Email": ["ese"],
-        "Target User": ["csc"],
-        "Alternate Target": ["dsd"],
-        "Questions": ["ese"]
-    }
-
-    # Write the dataframe to a CSV file
-    df = pd.DataFrame(dataframe)
-    df.to_csv("data.csv")
-
-    # Write dataframe to streamlit 
-    st.dataframe(df) 
-
-    # Allow the user to download the new CSV file
-    st.download_file(file, "data.csv")
+fields = [Nombre, Nota]
+ 
+with open('data.csv','a', newline='') as f:
+    writer = csv.writer(f)
+    writer.writerow(fields)
