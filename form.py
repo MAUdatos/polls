3f7cdd9499dd6ec0c2f586142e95b8ea77eac1d4
MAUdatos <121821660+MAUdatos@users.noreply.github.com>
2023-01-07 18:53:52 -0300

    Update form.py

--- form.py
+++ form.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import pandas as pd 
 
+f = open('data.csv','w')
+write = csv.writer(f)
+
 with st.form(key="my_form",clear_on_submit=True):
     
     st.write("Enter Note")
@@ -11,7 +14,7 @@ with st.form(key="my_form",clear_on_submit=True):
     submitted = st.form_submit_button("Submit")
     if submitted:
         st.write("Note", note_input, "stock_ticker", stock_ticker_input)
-        open('data.csv', 'a+').write(note_input,stock_ticker_input)
+        writer.writerow(note_input,stock_ticker_input)
 
 st.info(" #### Show contents of the CSV file :point_down:")
 st.dataframe(pd.read_csv("data.csv",names=["Stock","Note"]),height=300)
