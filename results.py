e67358c960ac7fef700d6cc6ab5d39aadc688a38
MAUdatos <121821660+MAUdatos@users.noreply.github.com>
2023-01-07 23:22:30 -0300

    Update results.py

--- results.py
+++ results.py
@@ -22,7 +22,7 @@ st.markdown(hide_table_row_index, unsafe_allow_html=True)
 
 # data
 df_poll = pd.read_csv('Sistematización y Mapeo MAU. Reacciones y visiones sobre futuros avances. .csv',sep=',').dropna(how = 'all')   # Base de datos respuestas poll 01.07.2023
-df_poll2 = df_poll.iloc[2,:]
+df_poll2 = df_poll.df.iloc[:,1:3] 
 
 #df_bbdd = df_bbdd.sort_values(by=['Organización_Huerta_Colectivo', 'Nombre_representante'])
 #df_bbdd.rename(columns = {'Latitud': 'lat', 'Longitud':'lon',},  inplace = True)  #obligatory names for the map
@@ -33,4 +33,4 @@ st.caption('<div style="text-align: left">Sistematización y Mapeo. Prototipo We
 
 st.header("Resultados de feedback sobre la herramienta. 2023.")
 st.markdown("  ")
-st.table(df_poll)
+st.table(df_poll2)
